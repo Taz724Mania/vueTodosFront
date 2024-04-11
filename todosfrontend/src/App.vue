0872686fdc37a1fdd7b1f138e217ed62158b2329
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-04-10 22:14:08 -0400

    vue frontend began

---
+++ todosfrontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>To Do:</h1>
+    <button>Add to Your List</button>
+    <ul>
+      <li v-for="todo in todos" v-bind:key="todo._id">
+        <h2>{{ todo.subject }}</h2>
+        <h3>{{ todo.details }}</h3>
+        <h3>{{ todo.dueDateTime }}</h3>
+        <h4>{{ todo.completed }}</h4>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import {ref, onBeforeMount} from 'vue'
+
+  const url = "https://vuetodos.onrender.com/todos"
+  const todos = ref([])
+
+  const getTodos = async function() {
+    const response = await fetch (url)
+    const data = await response.json()
+    todos.value = data
+  }
+
+  onBeforeMount(() => {getTodos()})
+</script>
+
+<style>
+
+</style>
